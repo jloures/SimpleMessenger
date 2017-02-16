083bae43de4355e7d7506375def00401033015e4
jloures <john_loures@hotmail.com>
2017-02-16 16:26:07 -0500

    Added package.json and basic server.js file

---
+++ server.js
@@ -0,0 +1,11 @@
+import express from 'express';
+const app = expres();
+const PORT = process.env.PORT || 9000;
+
+app.get('/', (req,res) => {
+    res.send('My messaging app!');
+});
+
+app.listen(PORT, () => {
+    console.log('Listening at port', PORT);
+})
